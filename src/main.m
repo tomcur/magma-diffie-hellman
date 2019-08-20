55886ad4689f49a4a5190934344a46fdd629c694
Thomas Churchman <thomas@kepow.org>
2019-08-20 20:37:32 +0200

    Add utility function to perform DH key exchange.

--- src/main.m
+++ src/main.m
@@ -1,7 +1,6 @@
 AttachSpec("spec");
 
 load 'math.m';
-load 'diffie_hellman.m';
 
 // Some checks:
 procedure curve_25519_check()
@@ -10,3 +9,24 @@ procedure curve_25519_check()
     assert ValidateSecurity(crv);
     print("Curve validated.");
 end procedure;
+
+function diffie_hellman_key_exchange(finite_cyclic_group)
+    dh := DiffieHellmanProtocol(finite_cyclic_group);
+
+    secrets := SetPartialSecrets(dh);
+    secret_alice := Random(secrets);
+    secret_bob := Random(secrets);
+    public_alice := PublicKey(secret_alice);
+    public_bob := PublicKey(secret_bob);
+    shared_alice := DiffieHellman(secret_alice, public_bob);
+    shared_bob := DiffieHellman(secret_bob, public_alice);
+
+    print
+        "Alice calculated shared secret:",
+        shared_alice;
+    print
+        "Bob calculated shared secret:",
+        shared_alice;
+
+    return (shared_alice`Point`Value eq shared_bob`Point`Value);
+end function;
